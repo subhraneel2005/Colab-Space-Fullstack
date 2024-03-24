1176eadb1858a6646a9edd82c5f39bbc6e84f22e
subhraneel2005 <digitalsubhraneel@gmail.com>
2024-03-24 19:33:34 +0530

    id checking through react toastify more work on ui later

--- frontend/colab-space/src/components/CreateRoom.jsx
+++ frontend/colab-space/src/components/CreateRoom.jsx
@@ -1,6 +1,8 @@
 import React, { useContext, useState } from 'react';
 import AppBar from './AppBar';
 import UseContext from '../contexts/UseContext';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 
 function CreateRoom() {
@@ -8,14 +10,25 @@ function CreateRoom() {
   const [roomName, setRoomName] = useState("")
   const {generateRandomID, randomID} = useContext(UseContext);
 
+  const enterRoom = () => {
+    if(!randomID && !roomName.trim()){
+        toast.error("Enter the credentials")
+    }
+    else{
+      toast.success(`You are being redirected to Room ${roomName} ID: ${randomID}`);
+    }
+  }
+
   return (
-    <div className='relative w-full min-h-screen flex justify-center items-center bg-gradient-to-r from-indigo-500 from-10% via-sky-500 via-30% to-emerald-500 to-90% ... select-none'>
+    <div className='relative w-full min-h-screen flex justify-center items-center bg-gradient-to-r from-indigo-500 from-10% via-sky-500 via-30% to-emerald-500 to-90% ...'>
+      <ToastContainer/>
         <div className='w-full h-full flex justify-center items-center'>
         <div className='w-full block'>
         <AppBar/>
         <div className='md:ml-[30%] ml-10'>
           <input 
           type="text"
+          value={roomName}
           placeholder='Room name'
           onChange={(e) => setRoomName(e.target.value)}
           className='px-4 py-2 rounded-md border-none outline-none bg-slate-800 text-white text-xl mb-4' />
@@ -23,8 +36,9 @@ function CreateRoom() {
           </div>
           <div className='block mt-20 overflow-hidden'>
             <h1 className='text-center text-white text-3xl'>Room Details: </h1>
-            <h1 className='select-auto text-center text-xl text-gray-200'>{`Room Name: ${roomName}`}</h1>
-            <h1 className='text-center text-gray-200 text-xl'>{`Room ID: ${randomID}`}</h1>
+            <h1 className='select-auto text-center text-xl text-gray-950'>{`Room Name: ${roomName}`}</h1>
+            <h1 className='text-center text-gray-950 text-xl'>{`Room ID: ${randomID}`}</h1>
+            <button className='ml-[27%] md:ml-[45%] mt-[40px] px-4 py-2 rounded-xl bg-gray-300 text-gray-950 text-xl hover:bg-gray-500 cursor-pointer' onClick={enterRoom}>Enter Room</button>
           </div>
         </div>
         </div>
